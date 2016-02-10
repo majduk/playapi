ae8e8596a60148858214ae35165de5fcaa8495d4
Michał Ajduk <majduk@gmail.com>
2016-02-10 12:05:14 +0100

    Update mnp_client.rb

--- lib/mnp_client.rb
+++ lib/mnp_client.rb
@@ -7,9 +7,7 @@ class MnpClient
       "show_imsi"=>"true",
       "target" => "#{msisdn}"
     }
-    uri = URI.parse( base_uri + "?output=json&show_imsi=true&appkey=" + appkey + "&target=" + msisdn)
-    result=get("",qparams) do |request,response|
+    return get("",qparams) do |request,response|
         return_nil_on_error(response)
-    return result  
   end
 end
